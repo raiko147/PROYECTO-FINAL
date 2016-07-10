6abcf7eea0b4751f514b30696d28668f1cad9469
Aldo <athom.x14@gmail.com>
2016-07-10 15:58:57 -0500

    Enunciado
    Enunciado

--- Programa para ISUR PY.py
+++ Programa para ISUR PY.py
@@ -1,10 +1,25 @@
-#Este programa esta diseñado para el control del ISUR
-#Aldo
+#Programa de Organización de Instituto Del Sur.
+#-------------------------Problema--------------------------------
+"""
+Este programa te alla
+1) El dato estadistico en porcentaje de alumnos aprobados (su ponderado mayor o igual 11) y desaprobados
+(su ponderado menor a 11) en todo el instituto, Abandonos, Matriculados
+2) Reporte de dinero recaudado por pensiones  de alumnos si:
+    -La pension es de 300 nuevos soles
+
+3) Reporte del pago de los Docentes que estan enseñando si se sabe:
+    -Son 11 Carreras y por cada carrera hay cuatro Docentes
+    -El pago por docente es de 1200 soles
+4) Reporte de gastos en cada mes teniendo como datos:
+    -Dinero con que cuenta el ISUR en (soles)
+    -Gastos
+
+
+
 
 """
-Este programa te alla el control de alumnos aprobados (su ponderado mayor o igual 11) y desaprobados (su ponderado menor a 11) en
+#-------------------Aldo(Inicio)-------------------
 
 
 
-
-"""
+#------------------Aldo(Final)---------------------
