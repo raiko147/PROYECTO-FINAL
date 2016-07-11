cee04e156b540636b7126ac713cff66cb9be9c0e
chrisr01 <christian_c_165@hotmail.com>
2016-07-11 16:52:49 -0500

    Update Programa para ISUR PY.py

--- Programa para ISUR PY.py
+++ Programa para ISUR PY.py
@@ -1,23 +1,39 @@
 #Programa de Organización de Instituto Del Sur.
 #-------------------------Problema--------------------------------
 """
+
+nos pide:
+-numeros de alumnos por carrera profesional
+-precio de pensiones de cada carrera profesional
+-precio de matriculas por alumno
+-salario de docentes
+-salario de administrativos
+-salario de personal de servicio
+-numero de alumnos aprobados
+-numero de alumnos desaprobados
 Este programa te alla
-1) El dato estadistico en porcentaje de alumnos aprobados (su ponderado mayor o igual 11) y desaprobados
+1)Reporte : 
+    1.-numero de alumnos(por cada carrera profesional), docentes, administrativos, personal de servicio
+    2.-total de pensiones por carrera profesional
+    3.-total de salario de docentes
+    4.-total de salario de administrativos
+    5.-total de salario de personal de servicio
+    6.-monto total recaudado por matriculas
+    7.-monto total recaudado por pagos de abandono
+2) El dato estadistico en porcentaje de alumnos aprobados (su ponderado mayor o igual 11) y desaprobados
 (su ponderado menor a 11) en todo el instituto, Abandonos, Matriculados
-2) Reporte de dinero recaudado por pensiones  de alumnos si:
-    -La pension es de 300 nuevos soles
-
-3) Reporte del pago de los Docentes que estan enseñando si se sabe:
-    -Son 11 Carreras y por cada carrera hay cuatro Docentes
-    -El pago por docente es de 1200 soles
-4) Reporte de gastos en cada mes teniendo como datos:
-    -Dinero con que cuenta el ISUR en (soles)
-    -Gastos
-
-
-
-
+3) Reporte de dinero recaudado por pensiones  de alumnos si:
+    -La pension es de 300 nuevos soles(segun cada carrera profesional)
+4) Reporte del pago de los Docentes que estan enseñando si se sabe:
+    -Son 11 Carreras y por cada carrera hay cuatro Docente
+    -El pago por docente es de 1200 soles(suposicion)
+5) Reporte financiero cada mes teniendo como datos:
+    -Monto de dinero con que cuenta el ISUR en (soles)[inicial]
+    -Monto de Gastos(salarios docentes, salarios administrativos, salarios personal de servicio)
+    -Monto de ingresos(pensiones, matriculas, monto de abandono)
+    total_dinero= monto_dinero_inicial + ingresos - gastos
 """
+#tambien podemos añadir personas postulantes , egresados, capacitaciones, eventos 
 #-------------------Aldo(Inicio)-------------------
 
 
