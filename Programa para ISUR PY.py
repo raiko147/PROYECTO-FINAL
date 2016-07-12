73b1dd101e81e2b975f607d8f4d531579e613e77
vinke294 <yanovivoyocristoviveenmi@hotmail.com>
2016-07-12 10:38:32 -0500

    Update Programa para ISUR PY.py

--- Programa para ISUR PY.py
+++ Programa para ISUR PY.py
@@ -57,3 +57,95 @@ numalum("Negocios internacionales")
 numalum("Diseño")
 numalum("Marketin")
 #------------------Aldo(Final)---------------------
+
+
+#-----------------KEVIN------------
+              INSTITUTO DEL SUR AREQUIPA
+                MENU:
+1.	¿Quiénes Somos?
+	Identidad
+Somos una comunidad educativa católica de alta especialización tecnológica, que a la luz de la espiritualidad sodálite, promueve integralmente la formación de la persona y el desarrollo de la sociedad
+Visión
+Seremos líderes con impacto nacional en la formación especializada de agentes comprometidos con el desarrollo integral de la sociedad.
+Pilares
+•	Comunidad educativa católica.
+•	Alta especialización tecnológica.
+•	Formación Integral de la persona humana.
+•	Evangelización y desarrollo integral de la sociedad.
+Valores
+•	Compromiso, adheridos plenamente a nuestra identidad y visión, teniendo como centro a la persona humana.
+•	Solidaridad, permanente actitud de servicio entre los miembros de nuestra comunidad educativa y hacia la sociedad.
+•	Integridad, pensar y actuar con virtud y coherencia.
+•	Excelencia, despliegue de nuestros talentos, según el máximo de nuestras capacidades y posibilidades.
+•	Innovación, incorporar creativamente las nuevas tecnologías.
+
+2.	Carreras Profesionales
+	Escuela de negocios
+	Administración de negocios bancarios y financieros 
+	Administración de negocios Internacionales
+	Marketing
+	Escuela de turismo
+	Administración de servicios de hostelería y restaurant
+	Guía oficial de turismo
+	Escuela de gastronomía
+	gastronomía
+	Escuela de tecnologías de la información 
+	Administración y sistemas
+	Desarrollo de sistema s de información 	
+	Escuela de diseño
+	Diseño gráfico multimedia
+	Diseño y decoración de interiores
+	Diseño de prendas de vestir
+
+3.	Campus Virtual
+Estimados alumnos:
+Según las modificaciones a la normatividad legal vigente emitidas por el Ministerio de Educación, que involucran a nuestras actividades académicas desde el semestre 2016-I, les informamos lo siguiente:
+1.	Matrícula 
+
+Los estudiantes que desaprueben una o más asignaturas PODRÁN SER PROMOVIDOS DE SEMESTRE; debiéndose matricular en primer lugar en las asignaturas desaprobadas y luego completar sus créditos con las asignaturas del semestre superior.
+2.	Titulación 
+
+Para poder obtener el Título Profesional Técnico, se deberá cumplir con los siguientes requisitos:
+o	Haber concluido y aprobado la totalidad de sus asignaturas.
+o	Haber cumplido con sus prácticas pre-profesionales.
+o	Haber aprobado el Proceso de Titulación en cualquiera de sus modalidades.
+o	ACREDITAR EL CONOCIMIENTO DE UN IDIOMA EXTRANJERO O LENGUA NATIVA.
+Con el objetivo de apoyar a nuestros alumnos en cumplir con este último requisito, el Instituto del Sur se encuentra trabajando en el lanzamiento de la LÍNEA DE INGLÉS SEMI PRESENCIAL, la cual se desarrollará en la plataforma virtual de PEARSON EDUCATION desde el próximo año. El costo, horarios y forma de matrícula serán comunicados oportunamente.
+Atentamente,	
+Martín Quintanilla Rodríguez
+Director Académico
+
+4.	Convenios Internacionales
+	Practicas Pre- profesionales y clases de campo
+	Capacitación empresarial
+	Incubación y emprendimiento de negocios
+	Capacitación en turismo 
+	Capacitación en tecnologías de la información 
+	Convalidación universitaria
+	Capacitación en gestión 
+5.	Contáctenos
+Gracias por visitar nuestra página web. Para comunicarse con nosotros llame a nuestra central telefónica: 6044444, anexo 100 o escríbanos al correo informes@isur.edu.pe, para temas de consultas sobre nuestras carreras técnico profesionales y sus procesos de admisión, sobre temas de extensión profesional, sugerencias u otra información que necesite concerniente a nuestros servicios.
+También lo atendemos personalmente de lunes a viernes de 08:30 a 13:00 h y de 16:00 a 19:30 h en el campus ubicado en Av. Salaverry 301, Vallecito, Arequipa.
+
+1)	POSTULANTES
+	Admisión 
+	Carreras profesionales
+	Carreras para gente que trabaja
+	Convenios
+	Ventajas Institucionales
+2)	ESTUDIANTES
+	Aulas virtuales
+	Servicios estudiantiles
+	Practicas pre-profesionales
+	Información bibliográfica
+	Información académica
+	Extensión profesional
+3)	EGRESADOS
+	ISUR emplea
+	Extensiones profesionales
+4)	EMPRESAS
+	ISUR emplea
+	Extensión profesional
+	Testimonios
+	convenios
+5)	EXTENSION PROFESIONAL
