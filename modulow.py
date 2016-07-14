540117dcba6106b6ca8b79916c5ce327c55f3279
Aldo <athom.x14@gmail.com>
2016-07-13 23:21:36 -0500

    Sincronizacion
    sincronizacion

--- modulow.py
+++ modulow.py
@@ -1,2 +1,13 @@
 class matriculas:
-    def --init--(self, matricula):
+    def __init__(self,matriculas):
+        self.matricula = matriculas
+    def prints(self):
+        print("ho")
+a=matriculas("hola")
+print(a.matricula)
+print(matriculas.prints)
+
+print("Prueba de sincronizacion")
+
+
+
