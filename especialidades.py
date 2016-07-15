c4459a6a3363d5bd1cf47335cf59f5224fc5bb4a
wil2016 <wil.gach@yandex.com>
2016-07-14 20:10:53 -0500

    contiene la informacion de especilidades

---
+++ especialidades.py
@@ -0,0 +1,8 @@
+def carreras():
+    carrerasProfesionales = ["Escuela de negocios", "Administración de negocios bancarios y financieros",
+                             "Administración de negocios Internacionales", "Marketing", "Escuela de turismo",
+                             "Administración de servicios de hostelería y restaurant", "Guía oficial de turismo",
+                             "Escuela de gastronomía", "Gastronomía", "Escuela de tecnologías de la información ",
+                             "Administración y sistemas", "Desarrollo de sistema s de información",
+                             "Escuela de diseño", "Diseño gráfico multimedia", "Diseño y decoración de interiores",
+                             "Diseño de prendas de vestir" ]
