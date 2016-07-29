899a66a657e1108a39f4427d0783daedfe2d69e0
wil2016 <wil.gach@yandex.com>
2016-07-29 11:13:21 -0500

    creacion de base datos y tablas

---
+++ creacion-base-datos.py
@@ -0,0 +1,18 @@
+import sqlite3
+import os,time
+os.system('clear')
+print (end="\tINSTITUTO DEL SUR AREQUIPA\nCreando base datos...")
+con=sqlite3.connect("trabajo-final.s3db")
+
+print("OK")
+time.sleep(1)
+os.system("clear")
+cursor=con.cursor()
+cursor.execute("CREATE TABLE registro(id integer primary key,nombre var(20) not null,"
+               "apellidos var(20) not null,edad int(2) not null,dni int(8) not null,direccion var(30) not null,"
+               "sexo var(1) not null,correo var(32) not null,especialidad var(60) not null,observaciones var(60));")
+#cerrar coneccion
+con.commit()
+print(end="\tCerrando coneccion base datos... ")
+con.close()
+print("ok")
