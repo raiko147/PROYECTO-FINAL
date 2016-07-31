e1cf94d39aa2573c1b40a311a932e302bb3041bd
vinke294 <yanovivoyocristoviveenmi@hotmail.com>
2016-07-30 19:12:54 -0500

    cursos y matriculas

--- creacion-base-datos.py
+++ creacion-base-datos.py
@@ -11,6 +11,15 @@ cursor=con.cursor()
 cursor.execute("CREATE TABLE registro(id integer primary key,nombre var(20) not null,"
                "apellidos var(20) not null,edad int(2) not null,dni int(8) not null,direccion var(30) not null,"
                "sexo var(1) not null,correo var(32) not null,especialidad var(60) not null,observaciones var(60));")
+con.commit()
+#crecion de tablas cursos
+#cursor.execute("CREATE TABLE cursos(codigo integer primary key,descripcion var(20) not null,"
+               "tipo_curso int(3) not null, observaciones var(60));")
+con.commit()
+#creacion de tabla matriculas
+#cursor.execute("CREATE TABLE matriculas(codigo_matricula integer primary key, ciclo int(20) not null,"
+               "semestre_academico int(1) not null, codigo_alumno int );")
+
 #cerrar coneccion
 con.commit()
 #creacion de tabla de pagos
