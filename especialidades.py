2e6cd0df6d9a84fedd688f6e90b0a43c9c86018a
julio1411 <julitto_14@hotmail.com>
2016-07-30 19:40:45 -0500

    agregando opciones menu principal
    estudiantes, docentes, personal administrativo, matriculas y pagos

--- especialidades.py
+++ especialidades.py
@@ -2,18 +2,21 @@ class especialidadesisur:
     def __init__(self):
         self.carrerasProfesionales = carrerrasProfesionales
     def carreras():
-        carrerasProfesionales = ["Escuela de negocios", "Administración de negocios bancarios y financieros",
-                                 "Administración de negocios Internacionales", "Marketing", "Escuela de turismo",
-                                 "Administración de servicios de hostelería y restaurant", "Guía oficial de turismo",
-                                 "Escuela de gastronomía", "Gastronomía", "Escuela de tecnologías de la información ",
-                                 "Administración y sistemas", "Desarrollo de sistema s de información",
-                                 "Escuela de diseño", "Diseño gráfico multimedia", "Diseño y decoración de interiores",
-                                 "Diseño de prendas de vestir" ]
+        carrerasProfesionales = ["Escuela de Negocios", "Administración de Negocios Nancarios y Financieros",
+                                 "Administración de Negocios Internacionales", "Marketing", "Escuela de Turismo",
+                                 "Administración de Servicios de Hostelería y Restaurant", "Guía oficial de Turismo",
+                                 "Escuela de Gastronomía", "Gastronomía", "Escuela de Tecnologías de la Información ",
+                                 "Administración y Sistemas", "Desarrollo de sistemas de Información",
+                                 "Escuela de Diseño", "Diseño Gráfico multimedia", "Diseño y Decoración de Interiores",
+                                 "Diseño de prendas de Vestir" ]
         return  carrerasProfesionales
 
     def opciones():
         opcionesTarea = ["Inscripciones","Reporte Estudiantes","Modificar Datos","Eliminar Registro","pagos"]
         return  opcionesTarea
+
+    def opcionesmenuprincipal(self):
+        opcionesTarea = ["Estudiantes", "Docentes", "Personal Administrativo", "maticulas", "Pagos"]
 
 
 
