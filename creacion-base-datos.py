588f79328bad3ddc1043054af976da5a169aacd5
julio1411 <julitto_14@hotmail.com>
2016-07-30 20:07:02 -0500

    Aporte tablas menu principal
    pagos, docentes y administrativos

--- creacion-base-datos.py
+++ creacion-base-datos.py
@@ -18,13 +18,25 @@ con.commit()
 con.commit()
 #creacion de tabla matriculas
 #cursor.execute("CREATE TABLE matriculas(codigo_matricula integer primary key, ciclo int(20) not null,"
-               "semestre_academico int(1) not null, codigo_alumno int );")
+                "semestre_academico int(1) not null, codigo_alumno int );")
 
-#cerrar coneccion
 con.commit()
-#creacion de tabla de pagos
-cursor.execute("CREATE TABLE pagos(codigo recibo integer primary key, concepto var(60) not null,"
-                "cantidad int(1) not null, nombre var(20));")
+#creacion de tabla pagos
+#cursor.execute("CREATE TABLA pagos(codigo_recibo integer primary key concepto var(60) not null,"
+                "cantidad int(1) not null, nombre var(20) );")
+
+con.commit()
+#creacion de tabla docentes
+#cursor.execute("CREATE TABLE docentes(id integer primary key, nombre var(20) not null,"
+                "apellidos var(20) not null, edad int(2) not null, dni int(8) not null, direccion var(30) not null,"
+                "sexo var(1) not null, correo var(32) not null, especialidad var(60) not null, observaciones var(60));")
+
+con.commit()
+#creacion de tabla administrativos
+#cursor.execute("CREATE TABLE docentes(id integer primary key, nombre var(20) not null,"
+                "apellidos var(20) not null, edad int(2) not null, dni int(8) not null, observaciones var(60));")
+con.commit()                
+#cerrar coneccion
 print(end="\tCerrando coneccion base datos... ")
 con.close()
 print("ok")
