c29d3eb1bc84aeeaf2ade753bb73887ee556569f
raiko147 <athom.x14@gmail.com>
2016-07-31 17:32:50 -0500

    Update modulow.py

--- modulow.py
+++ modulow.py
@@ -27,59 +27,5 @@ class matriculas:
             self.nombreConcatenado = self.nombreConcatenado[:-1]
         return self.nombreConcatenado   '''
 
-def menuAdministrativos():
-            print("\tMenu Administrativos")
-            opcion, j, opcionElegido, menuAlumno = " ", 1,"", especialidadesisur.opcionesAdministrativos()
-            for i in menuAlumno:
-                opcion += str(j)
-                j +=1
-            opcion += str(j)
-            while not (opcionElegido == opcion[-1]):#error string index out of range -- por que [-1] es la ultimacifra
-                j = 1
-                for i in menuAlumno:
-                    print("{0}.-{1}".format((j), i))
-                    j += 1
-                print("{}.-Salir de menu Administrativo".format(j))
-
-                opcionElegido = input("\tIngrese una opcion >> ".format(j))
-                if opcionElegido == "1":
-                    datos = inscripcionNueva()
-                    if datos :
-                        print(end="\tguardando... ")
-                        cursor.execute("insert into administrativos(nombre,apellidos,edad,dni,direccion,sexo,correo,especialidad,observaciones) "
-                                       "values(\"{0}\",\"{1}\",\"{2}\",\"{3}\",\"{4}\",\"{5}\",\"{6}\",\"{7}\",\"{8}\")"
-                                       "".format(datos[0],datos[1],datos[2],datos[3],datos[4],datos[5],datos[6],datos[7],datos[8]))
-                        con.commit()
-                        print("ok")
-                #reporte estudiantes
-                elif opcionElegido == "2":
-                    reporte("administrativos","codigo")
-                #modificar datos
-
-                elif opcionElegido == "3":
-                    print("\tModificacion de datos")
-                    modificar("administrativos","codigo")
-                #eliminar datos
-                elif opcionElegido == "4":
-                    print("\tEliminar registro")
-                    codigo = tablaCodigo("administrativo","codigo")
-                    codigo = codigo[-1]
-                    s = ""
-                    while not (s=="si" or s=="no" or s=="s" or s=="n" or codigo ==None ):
-                        s = input("Esta seguro que desea eliminar el codigo {} si/no: ".format(codigo)).lower()
-                        if s == "si" or s=="s" :
-                            print(end="\teliminando... ")
-                            cursor.execute("delete from administrativos where id =\'{}\'".format(codigo))
-                            con.commit()
-                            print("ok")
-                        elif s =="no" or s=="n" :
-                            print("\tcancelado")
-                        if not (s=="si" or s=="no"or s=="s" or s=="n"):
-                            print("\tconfirme correctamente")
-                if not (opcionElegido == opcion[-1]):
-                    print("Ingrese una opcion")
-
-            print()
-            os.system("clear")
 
 
